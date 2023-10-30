4c70ab0a83ba1c191fa8ab3111ac0ae08bb8c406
Ivan Dimitrov <ivan@idimitrov.dev>
2023-10-30 14:28:46 +0200

    nixvim

--- flake.nix
+++ flake.nix
@@ -1,7 +1,36 @@
 {
   description = "Flake that provides preconfigured IDEs";
 
-  outputs = { self, nixpkgs }: {
-
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
+
+  outputs = { self, nixvim, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nvim =
+          nixvim.legacyPackages."${system}".makeNixvim
+            { };
+        buildInputs = with pkgs; [
+          nvim
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          shellHook = ''
+        '';
+        };
+
+      });
 }
