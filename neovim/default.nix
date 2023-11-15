3dcce0a3540188a8761bad9775c8efe3fa31c26b
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-15 11:55:11 +0200

    GG

--- neovim/default.nix
+++ neovim/default.nix
@@ -36,78 +36,112 @@ let
     keymaps = [
       {
         mode = [ "n" "v" ];
-        action = "<Nop>";
         key = "<Space>";
+        action = "<Nop>";
         options.silent = true;
       }
       {
         mode = "n";
-        action = "<cmd>BufferNext<cr>";
         key = "<Tab>";
+        action = "<cmd>BufferNext<cr>";
         options.silent = true;
       }
       {
         mode = "n";
-        action = "<cmd>BufferPrevious<cr>";
         key = "<S-Tab>";
+        action = "<cmd>BufferPrevious<cr>";
         options.silent = true;
       }
       {
         mode = "n";
-        action = "<cmd>BufferClose<cr>";
         key = "<leader>x";
+        action = "<cmd>BufferClose<cr>";
         options.silent = true;
       }
       {
         mode = "n";
+        key = "<leader>/";
         action = "require('Comment.api').toggle.linewise.current";
         lua = true;
-        key = "<leader>/";
         options.silent = true;
       }
       {
         mode = "v";
-        action = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>";
         key = "<leader>/";
+        action = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>";
         options.silent = true;
       }
       {
         mode = [ "n" "t" ];
-        action = "<cmd>ToggleTerm<cr>";
         key = "<leader>h";
+        action = "<cmd>ToggleTerm<cr>";
         options.silent = true;
       }
       {
         mode = "n";
+        key = "<leader>ff";
         action = "require('telescope.builtin').find_files";
         lua = true;
-        key = "<leader>ff";
       }
       {
         mode = "n";
+        key = "<leader>fw";
         action = "require('telescope.builtin').live_grep";
         lua = true;
-        key = "<leader>fw";
       }
       {
         mode = "n";
+        key = "<leader>e";
         action = "vim.diagnostic.open_float";
         lua = true;
-        key = "<leader>e";
+      }
+      {
+        mode = "n";
+        key = "<leader>ca";
+        action = "vim.lsp.buf.code_action";
+        lua = true;
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>ca";
+        action = "vim.lsp.buf.rename";
+        lua = true;
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "vim.lsp.buf.definition";
+        lua = true;
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>lf";
+        action = "vim.lsp.buf.format";
+        lua = true;
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "K";
+        action = "vim.lsp.buf.hover";
+        lua = true;
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "gr";
+        action = "require('telescope.builtin').lsp_references";
+        lua = true;
+        options.silent = true;
       }
     ];
     plugins = {
       lsp = {
         enable = true;
         onAttach = ''
-          nmap("<leader>ca", vim.lsp.buf.code_action)
-          nmap("<leader>lr", vim.lsp.buf.rename)
-          nmap("gd", vim.lsp.buf.definition)
-          nmap("<leader>lf", function()
-              vim.lsp.buf.format()
-          end)
-          nmap("K", vim.lsp.buf.hover)
-          nmap("gr", require("telescope.builtin").lsp_references)
           if client.server_capabilities.documentHighlightProvider then
               vim.api.nvim_create_autocmd("CursorHold", {
                   buffer = bufnr,
