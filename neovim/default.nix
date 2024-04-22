ee33b8cf5e0a2200978af788681450f6419e5da6
Ivan Dimitrov <ivan@idimitrov.dev>
2024-04-22 17:35:28 +0300

    nvim cmp settings

--- neovim/default.nix
+++ neovim/default.nix
@@ -185,7 +185,22 @@ let
       };
       cmp = {
         enable = true;
-        settings.snippet.expand = "function(args) vim.snippet.expand(args.body) end";
+        settings = {
+          snippet.expand = "function(args) vim.snippet.expand(args.body) end";
+          mapping = {
+            "C-Space" = "cmp.mapping.complete()";
+            "C-d" = "cmp.mapping.scroll_docs(-4)";
+            "C-f" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+        };
       };
       lualine = {
         enable = true;
