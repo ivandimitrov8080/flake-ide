92fe2d2141898b3de2a29b335f7c92faf5030392
Ivan Dimitrov <ivan@idimitrov.dev>
2024-05-06 14:43:34 +0300

    default settings for vscode

--- vscode/default.nix
+++ vscode/default.nix
@@ -9,6 +9,34 @@ let
     enable = true;
     enableExtensionUpdateCheck = false;
     enableUpdateCheck = false;
+    userSettings = {
+      "editor.tabSize" = 2;
+      "editor.fontSize" = 14;
+      "editor.formatOnSave" = true;
+      "workbench.iconTheme" = "vscode-icons";
+      "editor.renderWhitespace" = "all";
+      "editor.minimap.enabled" = false;
+      "editor.renderLineHighlight" = "none";
+      "editor.mouseWheelZoom" = true;
+      "workbench.layoutControl.type" = "menu";
+      "workbench.editor.limit.enabled" = true;
+      "workbench.editor.limit.value" = 1;
+      "workbench.editor.limit.perEditorGroup" = true;
+      "workbench.editor.showTabs" = false;
+      "files.autoSave" = "onWindowChange";
+      "explorer.openEditors.visible" = 0;
+      "breadcrumbs.enabled" = false;
+      "editor.renderControlCharacters" = false;
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.activityBar.visible" = false;
+      "workbench.statusBar.visible" = false;
+      "editor.scrollbar.verticalScrollbarSize" = 2;
+      "editor.scrollbar.horizontalScrollbarSize" = 2;
+      "editor.scrollbar.vertical" = "hidden";
+      "editor.scrollbar.horizontal" = "hidden";
+      "workbench.layoutControl.enabled" = false;
+    };
+
   };
 in
 {
